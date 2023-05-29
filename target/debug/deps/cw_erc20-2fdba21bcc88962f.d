ffb0c1cff44641134472a7299251c57f342b7d36
Srikar <pasupuleti@antiersolutions.com>
2023-05-29 11:48:41 +0530

    first commit

---
+++ target/debug/deps/cw_erc20-2fdba21bcc88962f.d
@@ -0,0 +1,5 @@
+/data/cosmwasm/cw-erc20/target/debug/deps/cw_erc20-2fdba21bcc88962f.rmeta: src/lib.rs
+
+/data/cosmwasm/cw-erc20/target/debug/deps/cw_erc20-2fdba21bcc88962f.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/cw_erc20-d1af9e8b4103fabf.d
@@ -0,0 +1,5 @@
+/data/cosmwasm/cw-erc20/target/debug/deps/cw_erc20-d1af9e8b4103fabf.rmeta: src/lib.rs
+
+/data/cosmwasm/cw-erc20/target/debug/deps/cw_erc20-d1af9e8b4103fabf.d: src/lib.rs
+
+src/lib.rs:
